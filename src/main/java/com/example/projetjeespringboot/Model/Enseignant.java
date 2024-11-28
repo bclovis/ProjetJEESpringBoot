194a0af0dad93ace6000d66b5f3f8837b8dea702
lazyass78 <lazizi.yassine78@gmail.com>
2024-11-28 14:44:14 +0100

    Bean Enseignant

---
+++ src/main/java/com/example/projetjeespringboot/Model/Enseignant.java
@@ -0,0 +1,42 @@
+package com.example.projetjeespringboot.Model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "Enseignant")
+public class Enseignant {
+    @Id
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "nom", nullable = false)
+    private String nom;
+
+    @Column(name = "prenom", nullable = false)
+    private String prenom;
+
+    @Column(name = "dateNaissance", nullable = false)
+    private LocalDate dateNaissance;
+
+    @Column(name = "mdp", nullable = false)
+    private String mdp;
+
+    // Constructeurs
+    public Enseignant() {}
+    public Enseignant(String email, String nom, String prenom, LocalDate dateNaissance, String mdp) {
+        this.email = email;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.dateNaissance = dateNaissance;
+        this.mdp = mdp;
+    }
+}
