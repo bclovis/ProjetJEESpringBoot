1dc063eb25e21aaa98733115da59c464af565784
lazyass78 <lazizi.yassine78@gmail.com>
2024-11-29 01:01:46 +0100

    css de la page login et encore des bugs sur la partie gererEtudiants.html à corriger

--- src/main/java/com/example/projetjeespringboot/service/EtudiantService.java
+++ src/main/java/com/example/projetjeespringboot/service/EtudiantService.java
@@ -3,7 +3,11 @@ package com.example.projetjeespringboot.service;
 import com.example.projetjeespringboot.model.Etudiant;
 import com.example.projetjeespringboot.repository.EtudiantRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class EtudiantService {
@@ -18,5 +22,23 @@ public class EtudiantService {
             return etudiant;  // Retourne l'étudiant si l'email et le mot de passe sont valides
         }
         return null;  // Retourne null si l'authentification échoue
+    }
+
+    public List<Etudiant> getEtudiants(int page, int pageSize, String recherche) {
+        Pageable pageable = PageRequest.of(page - 1, pageSize);
+        if (recherche != null && !recherche.isEmpty()) {
+            return etudiantRepository.findByKeyword(recherche.toLowerCase(), pageable);
+        }
+        return etudiantRepository.findAll(pageable).getContent();
+    }
+
+    public int getTotalPages(int pageSize, String recherche) {
+        long totalResults;
+        if (recherche != null && !recherche.isEmpty()) {
+            totalResults = etudiantRepository.countByKeyword(recherche.toLowerCase());
+        } else {
+            totalResults = etudiantRepository.count();
+        }
+        return (int) Math.ceil((double) totalResults / pageSize);
     }
 }
